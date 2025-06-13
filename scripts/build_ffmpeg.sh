707e39c8f4d222fb094e0e7dcd9c28a41e084c8e
bingwen.ai <bingwen.ai@bytedance.com>
2025-06-13 14:02:41 +0800

    brew install cmake to 3.22.1 on macos

--- scripts/build_ffmpeg.sh
+++ scripts/build_ffmpeg.sh
@@ -19,7 +19,7 @@ install_deps() {
             echo 'Install example: >> /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
             exit
         fi
-        brew install automake cmake fdk-aac git lame libass libtool libvorbis libvpx opus sdl shtool texi2html theora wget x264 x265 xvid nasm
+        brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl shtool texi2html theora wget x264 x265 xvid nasm
     elif [[ "$(uname)" == "Linux" ]]; then
         # Ubuntu/Debian
         sudo apt-get update
@@ -27,7 +27,6 @@ install_deps() {
             autoconf \
             automake \
             build-essential \
-            cmake \
             git-core \
             libass-dev \
             libfreetype6-dev \
--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+_curfile=$(realpath $0)
+cur=$(dirname $_curfile)
 
 if [[ "$(uname)" == "Darwin"  ]];then
+    brew install --formula $cur/cmake.rb
     brew install opencv
 elif [[ "$(uname)" == "Linux" ]]; then
-    sudo apt update && sudo apt install -y libopencv-dev
+    sudo apt update && sudo apt install -y libopencv-dev cmake
 elif [[ "$(uname)" == *"_NT"* ]]; then
     echo "Not supported windows now."
 fi
